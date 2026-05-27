9cd7244bd44d83b2e651328031c0257557006b44
Subhajit Majumder <subhajit.winer@gmail.com>
2026-05-27 13:31:46 +0530

    demo files removed

--- myapi/src/services/example.service.ts
+++
@@ -1,8 +0,0 @@
-import { ExampleRepository } from "../repositories/example.repository";
-
-export class ExampleService {
-    constructor(private repository: ExampleRepository) { }
-    async ReturnFromService() {
-        return  await this.repository.getFromRepository();
-    }
-}
--- myapi/src/systems/types.ts
+++
@@ -1,5 +0,0 @@
-export const TYPES = {
-    ExampleRepository: Symbol("ExampleRepository"),
-    ExampleService: Symbol("ExampleService"),
-    ExampleController: Symbol("ExampleController"),
-};
